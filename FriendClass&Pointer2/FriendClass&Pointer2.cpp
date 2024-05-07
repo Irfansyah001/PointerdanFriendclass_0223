a554a21c68800b5b84aff447bcda1bf5142b8eff
Irfansyah001 <syahirfan545@gmail.com>
2024-05-07 15:31:55 +0700

    menambahkan class mahasiswa dan int nim void showNim dan menambahkan int main

--- FriendClass&Pointer2/FriendClass&Pointer2.cpp
+++ FriendClass&Pointer2/FriendClass&Pointer2.cpp
@@ -10,3 +10,18 @@ public:
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main()
+{
+    mahasiswa mhs{ 1 };  // Object mhs
+    mhs.showNim();  // member access operator
+
+    mahasiswa& refMhs = mhs;  // pointer reference refMhs
+    refMhs.nim = 2; // member access operator
+    mhs.showNim();
+
+    mahasiswa* pMhs = &mhs;  // pointer dereference pMhs
+    pMhs->nim = 3;  // Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
